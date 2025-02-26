98a5b26059e4ae6e8e0a80737ed6462b7542b61a
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-02-26 17:27:57 -0600

    Card Components Working

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import { faker } from '@faker-js/faker'
+    const firstname = faker.person.firstName()
+    const lastname = faker.person.lastName()
+    const jobtitle = faker.person.jobTitle()
+    const bio = faker.lorem.lines(2)
+    const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
+</script>
+
+
+
+<template>
+    <RouterLink :TO="{NAME: 'CardDetail', params: {id: username}}">
+        <div class="border border-gray-500 rounded-1g shadow h-full">
+            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.images.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
+            <div class="p-4">
+                <p class="mb-2 text-2x1 font-bold">{{ firstname }}  {{ lastname }}</p>
+                <p class="mb-3 font-normal text-gray-800">{{ jobtitle }}</p>
+                <p class="mb-3 font-normal text-gray-800">{{ bio }}</p>
+            </div>
+
+        </div>
+    </RouterLink>
+
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Hello There ✋')
+import MainCards from '@/components/MainCards.vue';
+import MainPagination from '@/components/MainPagination.vue';
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <MainCards></MainCards> 
+  <MainPagination></MainPagination>
 </template>
