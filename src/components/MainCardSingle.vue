65b3f19516e44b7cc3dd55fd83df864fb718f0b6
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-03-01 14:21:53 -0600

    Card Components working

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { faker } from '@faker-js/faker';
+    const firstname = faker.person.firstname()
+    const lastname = faker.person.lastname()
+     
+
+</script>
+
+<template>
+    {{ firstname }}  {{ lastname }} <br/>
+</template>
