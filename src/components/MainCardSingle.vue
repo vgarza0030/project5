55f57d2b5ea579bc7c99ba5811a6940263259eea
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-03-01 19:19:32 -0600

    Finished Navigation Menu

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -11,7 +11,7 @@ import { faker } from '@faker-js/faker'
 
 <template>
     <div class="border border-gray-500 rounded-1g shadow bg-gray-400 h-full">
-        <img class="object-fill h-480 w-960" v-bind:src="faker.image.urlLoremFlickr({category: 'cakes', height:480, width:960})" />
+        <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cakes', height:480, width:960})" />
         <div class="p-4">
             <p class="mb-2 text-2xl font-bold">{{ firstname }} {{ lastname }}</p>
             <p class="mb-3 font-normal text-gray-800">{{ jobtitle }}</p>
